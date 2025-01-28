bf73a6d184d67bca1ffdd23082ca3fa6f05e76a0
MaxouLeKangou <maxime.legrand@bessarion.fr>
2025-01-28 14:59:21 +0100

    feat(config): Add hosting mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
