340f065fa0997a6f70d9c8c7995a50346d40d15e
MaxouLeKangou <maxime.legrand@bessarion.fr>
2025-02-05 19:04:28 +0100

    feat(database): Create Tool type

---
+++ types/tool.ts
@@ -0,0 +1,14 @@
+export type Tool = {
+    collectionId: string;
+    collectionName: string;
+
+    id: string;
+    thumbnail: string;
+    title: string;
+    society: string;
+    description: string;
+    tags: [];
+    likes: [];
+
+    created: string;
+}
