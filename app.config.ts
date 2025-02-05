f1491da97ab4bca7f67690174be91bbc8127b1cb
MaxouLeKangou <maxime.legrand@bessarion.fr>
2025-02-05 15:17:36 +0100

    fix(config): Change Button & Input Component

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,41 @@
 export default defineAppConfig({
     ui: {
-        strategy: 'override',
+        strategy: "override",
 
         button: {
-            compoundVariants: [
-                {
-                  variant: 'solid',
-                  class: 'font-semibold text-[0.875rem] px-6 py-2.5 rounded-full cursor-pointer lg:text-[1rem] lg:px-8 md:py-3'
+            base: 'cursor-pointer',
+
+            variants: {
+                color: {
+                    primary: 'bg-primary text-dark-100',
+                    white: 'bg-light-100 !text-dark-100 !rounded-full transition hover:opacity-75'
                 },
-                {
-                    variant: 'outline',
-                    class: 'font-semibold text-[0.875rem] px-6 py-2.5 rounded-full cursor-pointer lg:text-[1rem] lg:px-8 md:py-3'
+                size: {
+                    sm: {
+                        base: 'font-semdibold text-m-sm px-4 lg:px-6 lg:py-2.5 lg:text-d-sm lg:rounded-xl'
+                    },
+                    md: {
+                        base: 'font-semibold text-m-md px-6 py-2.5 rounded-full lg:text-d-sm lg:px-8 md:py-3'
+                    }
+                },
+                variant: {
+                    outline: 'bg-transparent text-light-100'
                 }
-            ]
+            }
+        },
+
+        input: {
+            variants: {
+                color: {
+                    white: 'text-light-100 placeholder:text-light-200 !ring-light-100/10 focus:!ring-light-100/25'
+                },
+                size: {
+                    md: {
+                        base: 'rounded-md pl-5 pr-10 py-2.5 !text-d-sm',
+                        trailing: 'pe-1.5',
+                    }
+                }
+            }
         }
     }
-})
+});
